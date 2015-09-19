908ddf5bfda3b38bcef9265a5b834d5c2c3bbb0f
hansemannn <hans.knoechel@hs-osnabrueck.de>
2015-09-18 17:15:36 -0700

    Add gitignore, cleanup build directory

--- iphone/build/TiLineaPro.build/Release-iphonesimulator/TiLineaPro.build/Objects-normal/x86_64/DeInfoteamLineaproModuleAssets.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/hans/Documents/Apps/TiLineaPro/iphone/Classes/DeInfoteamLineaproModuleAssets.m \
-  /Users/hans/Documents/Apps/TiLineaPro/iphone/Classes/DeInfoteamLineaproModuleAssets.h
